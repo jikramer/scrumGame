5602ed91b80c7b694c49115d2bc47614f3be4631
Yashodha15 <yashodhasavadi@gmail.com>
2017-03-30 14:01:06 -0400

    Added a GetQuestionDao to retrieve MCQ from the datbase. Modified the Dbinit script to add MCA and answer table.

--- scripts/dbinit .sql
+++ scripts/dbinit .sql
@@ -167,3 +167,57 @@ insert into facultystudent values(1,0)
 
 ALTER TABLE `scrumgame`.`user_questionaire_detail` 
 ADD COLUMN `last_update_dt` VARCHAR(45) NULL AFTER `score`;
+
+# Q&A
+drop table scrumgame.MultipleChoiceQuestion;
+
+create table scrumgame.MultipleChoiceQuestion(
+Question_ID int auto_increment,
+Question varchar(1000),
+choice_1 varchar(500),
+choice_2 varchar(500),
+choice_3 varchar(500),
+choice_4 varchar(500),
+Question_type varchar(30),
+CHECK(Question_type in ("Beginner","Intermediate","Expert")),
+PRIMARY KEY(Question_ID)
+);
+
+insert into scrumgame.MultipleChoiceQuestion (Question,choice_1,choice_2,choice_3,choice_4,Question_type) values ("Where are the customer requirements stored?","In the Product Backlog","In the Sprint Backlog","In a database","In a Scrum Product Requirement Specification","Beginner");
+insert into scrumgame.MultipleChoiceQuestion (Question,choice_1,choice_2,choice_3,choice_4,Question_type) values ("Which concept is NOT defined in the Scrum Framework?","Scrum Master","Project Manager","Scrum Product Owner","Scrum Product Burndown","Beginner");
+Insert into scrumgame.MultipleChoiceQuestion (Question,choice_1,choice_2,choice_3,choice_4,Question_type)  values ("What kind of software development projects can be executed by Scrum Project Management Framework?","Complete software packages","Customer projects","Sub-systems, components or parts of bigger systems","All kinds of software development projects","Intermediate");
+Insert into scrumgame.MultipleChoiceQuestion (Question,choice_1,choice_2,choice_3,choice_4,Question_type) values ("What does NOT belong to cornerstones of the agile manifesto?","Individuals and interactions over processes and tools","Working software over comprehensive documentation","Processes over people","Responding to change over following a plan","Intermediate");
+Insert into scrumgame.MultipleChoiceQuestion (Question,choice_1,choice_2,choice_3,choice_4,Question_type) values ("What are the advantages of the Scrum Framework?","Fine-grained requirements are only defined when they are really needed.","All activities to design, build and test a certain functionality are kept together in one phase.","Changes are expected and welcomed by Scrum team.","All of the given answers","Expert");
+
+#SP that returns Multiple Choice Question
+drop procedure getQuestion;
+DELIMITER $$
+Create procedure getQuestion(
+IN levels varchar(30))
+begin
+(Select Question,choice_1,choice_2,choice_3,choice_4 from scrumgame.MultipleChoiceQuestion where Question_type = levels);
+ END$$
+DELIMITER ;
+
+
+# Answer for the qestion
+drop table scrumgame.QandA;
+
+create table  scrumgame.QandA(
+Question_ID int,
+Answer char(1),
+FOREIGN KEY(Question_ID) REFERENCES scrumgame.MultipleChoiceQuestion(Question_ID)
+);
+
+Insert into scrumgame.QandA values(1,A);
+Insert into scrumgame.QandA values(2,B);
+Insert into scrumgame.QandA values(3,D);
+Insert into scrumgame.QandA values(4,C);
+Insert into scrumgame.QandA values(5,D);
+Insert into scrumgame.QandA values(5,D);
+
+
+
+
+
+
---
+++ src/main/java/dao/GetQuestionDao.java
@@ -0,0 +1,88 @@
+import java.sql.*;
+import model.User;
+import model.UserDetails;
+import utils.DBUtils;
+
+public class getQuestion {
+public void getMCQ(User user){
+		
+		Connection conn = DBUtils.getConnection();
+		
+		int level = user.getUserDetails().getQuestionaireLevel();
+		
+		switch(level){
+		case 1 : 
+			try{
+				
+				CallableStatement cs = conn.prepareCall("{Call getQuestion(?)}");
+				cs.setString(1,"Beginner");
+				ResultSet rs = cs.executeQuery();
+				while(rs.next()){
+				System.out.println(rs.getString("question") + "\na)" + rs.getString("choice_1") +"\nb)"+rs.getString("choice_2")+ "\nc)" + rs.getString("choice_3")+"\nd)"+rs.getString("choice_4")+"\n");
+				}
+				rs.close();
+				cs.close();
+				}
+				catch(Exception e)
+				 {
+				      System.err.println("Got an exception! ");
+				      System.err.println(e.getMessage());
+				    }
+			break;
+	case 2 : 
+		try{
+			
+			CallableStatement cs = conn.prepareCall("{Call getQuestion(?)}");
+			cs.setString(1,"Intermediate");
+			ResultSet rs = cs.executeQuery();
+			while(rs.next()){
+			System.out.println(rs.getString("question") + "\na)" + rs.getString("choice_1") +"\nb)"+rs.getString("choice_2")+ "\nc)" + rs.getString("choice_3")+"\nd)"+rs.getString("choice_4")+"\n");
+			}
+			rs.close();
+			cs.close();
+			}
+			catch(Exception e)
+			 {
+			      System.err.println("Got an exception! ");
+			      System.err.println(e.getMessage());
+			    }
+		break;
+	case 3 : 
+		try{
+			
+			CallableStatement cs = conn.prepareCall("{Call getQuestion(?)}");
+			cs.setString(1,"Expert");
+			ResultSet rs = cs.executeQuery();
+			while(rs.next()){
+			System.out.println(rs.getString("question") + "\na)" + rs.getString("choice_1") +"\nb)"+rs.getString("choice_2")+ "\nc)" + rs.getString("choice_3")+"\nd)"+rs.getString("choice_4")+"\n");
+			}
+			rs.close();
+			cs.close();
+			}
+			catch(Exception e)
+			 {
+			      System.err.println("Got an exception! ");
+			      System.err.println(e.getMessage());
+			    }
+		break;
+	 default:  break;	
+		}
+	
+	
+			
+	}
+		
+	public static void main(String args[]){
+		getQuestion q = new getQuestion();
+		User user = new User();
+		user.setId(2);
+		UserDetails d = new UserDetails();
+		d.setQuestionaireLevel(1);
+		user.setUserDetails(d);
+		user.setUserName("aquaman");
+		q.getMCQ(user);
+	
+				
+	}
+		
+}
